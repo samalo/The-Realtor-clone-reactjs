dcc771c0d5574939f6dd350b7b737dc55102b164
Sam Mhondiwa <smhonds@gmail.com>
2023-10-29 21:47:01 +0200

    Created react app, Installed Tailwind css, installed browser router, and created 6 pages

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
